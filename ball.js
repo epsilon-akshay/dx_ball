993ef7920ef251752777e3209446c54cb3e82964
akshay.nambiar <akshay.nambiar@go-jek.com>
2020-09-03 02:01:29 +0530

    move ball and base

--- ball.js
+++ ball.js
@@ -2,7 +2,9 @@ var ball = {
     x: 400,
     y: 400,
     color: 'red',
-    radius: 10
+    radius: 10,
+    spdX: 5,
+    spdY: 5,
 }
 
 var drawBall = function(ctx) {
@@ -12,4 +14,16 @@ var drawBall = function(ctx) {
     ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI)
     ctx.fill()
     ctx.restore()
+}
+
+var moveBall = function(ctx, canvas) {
+    if (ball.x == 0 || ball.x == (canvas.width - 2 * ball.radius)) {
+        ball.spdX = -ball.spdX
+    }
+    if (ball.y == 0 || ball.y == (canvas.height - 2 * ball.radius)) {
+        ball.spdY = -ball.spdY
+    }
+    ball.x = ball.x + ball.spdX
+    ball.y = ball.y + ball.spdY
+    drawBall(ctx)
 }
--- base.js
+++ base.js
@@ -3,12 +3,34 @@ var base = {
     y: 800,
     color: 'blue',
     width: 150,
-    height: 20
+    height: 20,
+    movingLeft: false,
+    movingRight: false
 }
+var SPEED = 5
 
 var drawBase = function(ctx) {
     ctx.save()
     ctx.fillStyle = base.color
     ctx.fillRect(base.x, base.y, base.width, base.height)
     ctx.restore()
+}
+
+var moveBase = function(ctx, canvas) {
+    if (base.x < 0) {
+        base.x = 0
+    }
+
+    if (base.x > (canvas.width - base.width)) {
+        base.x = (canvas.width - base.width)
+    }
+    if (base.movingLeft == true) {
+        base.x = base.x - SPEED
+    }
+    if (base.movingRight == true) {
+        base.x = base.x + SPEED
+    }
+
+
+    drawBase(ctx)
 }
